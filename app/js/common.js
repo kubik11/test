bd14ec66bd21595acc50cf342a9a0f45bfd78618
kubik11 <kubyshkin2@gmail.com>
2017-09-29 00:15:29 +0400

    test

--- app/js/common.js
+++ app/js/common.js
@@ -11,14 +11,34 @@ $(document).ready(function(){
   	autoplayHoverPause: true,
   	nav: true,
   	smartSpeed: 1000,
-  	navText: ["<i class='fa fa-angle-left' aria-hidden='true'></i>"
-,"<i class='fa fa-angle-right' aria-hidden='true'></i>"
-]
-  	//autoWidth: true
-  });
-
-var arr = $('.navigation a');
-
- });
+  	navText: ["<i class='fa fa-angle-left' aria-hidden='true'></i>","<i class='fa fa-angle-right' aria-hidden='true'></i>"],
+    responsive : {
+    // breakpoint from 0 up
+    0 : {
+           dots: true,
+           nav: false
+        },
+        // breakpoint from 480 up
+        870 : {
+          dots: false,
+          nav: true
+        },
+    }
+   });
 //Products carousel
+  resS();
+});
+
+
+window.onresize = resS;
+ // var currentHam = document.querySelector('.inner-top-line');
+  function resS(){
+    screenW = window.innerWidth ;
+    if(screenW < 700){
+      $('.hamburger').css('display', 'inline-block');
+    }
+    if(screenW > 700){
+      $('.hamburger').css('display', 'none');
+    }
+  }
 });
