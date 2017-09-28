8c32bc6def2c6e70afcbc20ad70d23563fe7833b
Herr-Gunter <kubyshkin1990@mail.ru>
2017-09-28 12:44:03 +0300

    test

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,24 @@
 $(function() {
 
 	// Custom JS
+	//===================================SLIDER============================//
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+  	loop: true,
+  	items: 1,
+  	autoplay: true,
+  	autoplayHoverPause: true,
+  	nav: true,
+  	smartSpeed: 1000,
+  	navText: ["<i class='fa fa-angle-left' aria-hidden='true'></i>"
+,"<i class='fa fa-angle-right' aria-hidden='true'></i>"
+]
+  	//autoWidth: true
+  });
+
+
+ });
+//Products carousel
 
 });
